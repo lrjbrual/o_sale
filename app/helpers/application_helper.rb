bd9414352eff74b9b413fc894ceb5728916e3e7e
Ryan Jeff Brual <lrjbrual@live.com>
2019-12-16 07:05:43 +0100

    Update the new sale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
   def commented_by(user)
     user.fullname # passed it call it from model in user
   end
+
+  def persisted_comments(comments)
+    comments.reject { |cmt| cmt.new_record? }
+  end
   
 end
